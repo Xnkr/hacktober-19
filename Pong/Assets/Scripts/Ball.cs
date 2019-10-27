694bb56af2f7fbd9a54f64eb7c5ec0637dc064c3
Xnkr <26054028+Xnkr@users.noreply.github.com>
2019-10-26 23:12:37 -0500

    Working Pong

---
+++ Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    Vector3 velocity;
+    [Range(0,1)]
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        ResetPosition();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        velocity = velocity.normalized * speed;
+        transform.position += velocity;
+    }
+
+    void ResetPosition(){
+        transform.position = Vector3.zero;
+        float z = Random.Range(0, 2) * 2f - 1f;
+        float x = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
+        velocity = new Vector3(x, 0, z);
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        switch(collision.transform.name){
+            case "Bound East":
+            case "Bound West":
+                velocity.x *= -1f;
+                break;
+            case "Bound North":
+            case "Bound South":
+                ResetPosition();
+                break;
+            case "Player Paddle":
+            case "AI Paddle":
+                velocity.z *= -1f;
+                break;
+        }
+    }
+}
---
+++ Pong/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Camera playerCam;
+
+    void Update()
+    {
+        if(Input.touchCount != 1){
+            return;
+        }
+        var ray = playerCam.ScreenPointToRay(Input.touches[0].position);
+        var hitInfo = new RaycastHit();
+        if(Physics.Raycast(ray, out hitInfo)){
+            if (hitInfo.transform.name != "Bound South"){
+                return;
+            }
+            var x = hitInfo.point.x;
+            var newPos = transform.position;
+            newPos.x = x;
+            transform.position = newPos;
+        }
+
+        
+    }
+}
